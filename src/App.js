e621ec0d4f13501e0e21da44ecf4ae1e01272b94
ceosss <sswarajsamant@gmail.com>
2020-05-15 20:29:53 +0530

    added cartIcon and cartDropdown components to the app, also implemented cart reducer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 
 import Homepage from "./pages/homepage/Homepage.component";
@@ -42,15 +42,30 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={Homepage} />
           <Route exact path="/shop" component={ShopPage} />
-          <Route exact path="/signin" component={SignInAndSignUpPage} />
+          {/* <Route exact path="/signin" component={SignInAndSignUpPage} /> */}
+          <Route
+            exact
+            path="/signin"
+            render={() =>
+              this.props.currentUser ? (
+                <Redirect to="/" />
+              ) : (
+                <SignInAndSignUpPage />
+              )
+            }
+          />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser,
+});
+
 const mapDispatchToPorps = (dispatch) => ({
   setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 });
 
-export default connect(null, mapDispatchToPorps)(App);
+export default connect(mapStateToProps, mapDispatchToPorps)(App);
