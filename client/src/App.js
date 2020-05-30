c536f961e166ccc5d6c12d42cbba3b586662feff
ceosss <sswarajsamant@gmail.com>
2020-05-30 23:02:05 +0530

    removed unwanter code from app

--- client/src/App.js
+++ client/src/App.js
@@ -46,29 +46,6 @@ const App = ({ setCurrentUser, currentUser }) => {
     });
   }, [setCurrentUser]);
 
-  // componentDidMount() {
-
-  //   const { setCurrentUser } = this.props;
-  //   this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
-  //     if (userAuth) {
-  //       const userRef = await createUserProfileDocument(userAuth);
-
-  //       userRef.onSnapshot((snapShot) => {
-  //         setCurrentUser({
-  //           id: snapShot.id,
-  //           ...snapShot.data(),
-  //         });
-  //       });
-  //     }
-  //     setCurrentUser(userAuth);
-  //     // SetCollections("collections", collectionsArray);
-  //   });
-
-  // }
-
-  // componentWillUnmount() {
-  //   this.unsubscribeFromAuth();
-  // }
   return (
     <div>
       <GlobalStyles />
